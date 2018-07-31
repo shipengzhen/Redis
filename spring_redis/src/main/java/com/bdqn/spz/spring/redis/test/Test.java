dfdaee2142f96cdea31d65ee6305f59864535b9b
spz <473585505@qq.com>
2018-07-31 10:21:25 +0800

    更新

--- spring_redis/src/main/java/com/bdqn/spz/spring/redis/test/Test.java
+++ spring_redis/src/main/java/com/bdqn/spz/spring/redis/test/Test.java
@@ -1,5 +1,7 @@
 package com.bdqn.spz.spring.redis.test;
 
+import java.io.IOException;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,34 +12,45 @@ import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 public class Test {
-	
-	public static void main(String[] args) {
-		
-//		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext-redis.xml");
-//		
-//		RedisTemplate redisTemplate=context.getBean("redisTemplate",RedisTemplate.class);
-//		
-//		redisTemplate.execute(new RedisCallback<String>() {
-//			@Override
-//			public String doInRedis(RedisConnection connection) throws DataAccessException {
-//				System.out.println(connection.get("name".getBytes()).toString());
-//				return null;
-//			}
-//		});
-	    ShardedJedisClient shardedJedisClient=new ShardedJedisClient();
-	    
-	    System.out.println(shardedJedisClient.set("name","spz11"));
-	    
-	    @SuppressWarnings("resource")
+
+    public void ShardedJedisClientTest() {
+        ShardedJedisClient shardedJedisClient = new ShardedJedisClient();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(shardedJedisClient.set("name" + i, "spz" + i));
+            System.out.println(shardedJedisClient.get("name" + i));
+        }
+    }
+
+    public void JedisPoolTest() {
+        @SuppressWarnings("resource")
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-redis.xml");
-	    
-	    JedisCluster jedisCluster = context.getBean("jedisCluster", JedisCluster.class);
-	    System.out.println(jedisCluster.get("name"));
-	    
-	    JedisPool pool = context.getBean("jedisPool", JedisPool.class);
+        JedisPool pool = context.getBean("jedisPool", JedisPool.class);
         Jedis jedis = pool.getResource();
-        System.out.println(jedis.set("name","spz22"));
-        System.out.println(jedis.get("name"));
-		
-	}
+        for (int i = 0; i < 100; i++) {
+            System.out.println(jedis.set("name" + i, "spz" + i));
+            System.out.println(jedis.get("name" + i));
+        }
+    }
+
+    public void JedisClusterTest() throws IOException {
+        @SuppressWarnings("resource")
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-redis.xml");
+        JedisCluster jedisCluster=null;
+        try {
+             jedisCluster = context.getBean("jedisCluster", JedisCluster.class);
+            for (int i = 0; i < 100; i++) {
+                System.out.println(jedisCluster.set("name" + i, "spz" + i));
+                System.out.println(jedisCluster.get("name" + i));
+            }
+        }catch (Exception e) {
+           e.printStackTrace();
+        } finally {
+            jedisCluster.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        new Test().JedisClusterTest();
+    }
 }
